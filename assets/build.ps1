0bcc94cba35fd2c7defe110efa0eae3563c2a6df
George Olson <georgeo@slalom.com>
2017-05-26 15:59:54 -0700

    Updated package information

--- assets/build.ps1
+++ assets/build.ps1
@@ -1,7 +1,7 @@
-cd $PSScriptRoot\..
+cd "C:\Source\Patolus - Treatment API"
 
 SonarQube.Scanner.MSBuild.exe begin /d:sonar.host.url=https://sonarqube.patolus.io /k:"patolus-treatment" /n:"Patolus Treatment" /v:"1.0" /d:sonar.login=451d0eae1a13c9e2a8c92b547942435c23933393
 
-& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" /t:Rebuild
+& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" /t:Rebuild /property:Configuration=Debug
 
 SonarQube.Scanner.MSBuild.exe end
